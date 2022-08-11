1653e20e0dae4c113ca3964a42b48f2c14aaee46
neil <zding501@gmail.com>
2022-08-11 12:05:02 +1000

    feat(Algorithm): remove element method 2 double pointer

--- 27.remove-element.ts
+++ 27.remove-element.ts
@@ -5,29 +5,40 @@
  */
 
 // @lc code=start
+// function removeElement(nums: number[], val: number): number {
+//     let ans = 0;
+//     for (let i = 0; i < nums.length; i++) {
+//         // console.log('nums[i]: ', nums[i]);
+//         if (nums[i] === val) {
+//             for (let t = nums.length - 1; t > i; t--) {
+//                 // console.log('nums[t]: ', nums[t]);
+//                 if (nums[t] !== val) {
+//                     let temp = nums[t];
+//                     nums[t] = nums[i];
+//                     nums[i] = temp;
+//                     // console.log('nums: ', nums);
+//                     break;
+//                 }
+//             }
+//         }
+//     }
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i] !== val) {
+//             ans += 1;
+//         }
+//     }
+//     return ans;
+// };
+
 function removeElement(nums: number[], val: number): number {
-    let ans = 0;
-    for (let i = 0; i < nums.length; i++) {
-        // console.log('nums[i]: ', nums[i]);
-        if (nums[i] === val) {
-            for (let t = nums.length - 1; t > i; t--) {
-                // console.log('nums[t]: ', nums[t]);
-                if (nums[t] !== val) {
-                    let temp = nums[t];
-                    nums[t] = nums[i];
-                    nums[i] = temp;
-                    // console.log('nums: ', nums);
-                    break;
-                }
-            }
+    let left = 0;
+    for (let right = 0; right < nums.length; right++) {
+        if (nums[right] !== val) {
+            nums[left] = nums[right];
+            left++;
         }
     }
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] !== val) {
-            ans += 1;
-        }
-    }
-    return ans;
+    return left;
 };
 // @lc code=end
 
