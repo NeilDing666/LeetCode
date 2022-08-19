387824f1ac9a866fc62ec426c6734e73d3333213
neil <zding501@gmail.com>
2022-08-19 16:20:31 +1000

    feat(Algorithm): Greedy

---
+++ 881.boats-to-save-people.ts
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=881 lang=typescript
+ *
+ * [881] Boats to Save People
+ */
+
+// @lc code=start
+function numRescueBoats(people: number[], limit: number): number {
+    let ans = 0;
+
+    people.sort((a, b) => a - b);
+
+    for (let i = people.length - 1; i >= 0; i--) {
+        if (people[i] && i === 0) {
+            ans++;
+        } else if (!people[i]) {
+            continue;
+        }
+        if (people[i] === limit) {
+            ans++;
+        } else {
+            for (let t = i - 1; t >= 0; t--) {
+                if (people[t] && people[t] <= limit - people[i]) {
+                    ans++;
+                    people[t] = NaN;
+                    break;
+                } else if (t === 0) {
+                    ans++;
+                    break;
+                }
+            }
+        }
+    }
+
+    return ans;
+};
+// @lc code=end
+
