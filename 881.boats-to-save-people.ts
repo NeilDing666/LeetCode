fba6f57ad92ec21e9098499d5acb0c6e0a1f4ddc
neil <zding501@gmail.com>
2022-08-19 16:44:20 +1000

    feat(Algorithm): Optimize by using double pointer

--- 881.boats-to-save-people.ts
+++ 881.boats-to-save-people.ts
@@ -5,34 +5,63 @@
  */
 
 // @lc code=start
+// function numRescueBoats(people: number[], limit: number): number {
+//     let ans = 0;
+
+//     people.sort((a, b) => a - b);
+
+//     for (let i = people.length - 1; i >= 0; i--) {
+//         if (people[i] && i === 0) {
+//             ans++;
+//         } else if (!people[i]) {
+//             continue;
+//         }
+//         if (people[i] === limit) {
+//             ans++;
+//         } else {
+//             for (let t = i - 1; t >= 0; t--) {
+//                 if (people[t] && people[t] <= limit - people[i]) {
+//                     ans++;
+//                     people[t] = NaN;
+//                     break;
+//                 } else if (t === 0) {
+//                     ans++;
+//                     break;
+//                 }
+//             }
+//         }
+//     }
+
+//     return ans;
+// };
+
+// Optimization
 function numRescueBoats(people: number[], limit: number): number {
     let ans = 0;
 
     people.sort((a, b) => a - b);
 
+    let h = people.length - 1;
+    let l = 0;
     for (let i = people.length - 1; i >= 0; i--) {
-        if (people[i] && i === 0) {
+
+        if (h === l) {
             ans++;
-        } else if (!people[i]) {
-            continue;
+            break;
         }
-        if (people[i] === limit) {
+
+        if (people[h] + people[l] <= limit) {
             ans++;
+            l++;
+            i--;
         } else {
-            for (let t = i - 1; t >= 0; t--) {
-                if (people[t] && people[t] <= limit - people[i]) {
-                    ans++;
-                    people[t] = NaN;
-                    break;
-                } else if (t === 0) {
-                    ans++;
-                    break;
-                }
-            }
+            ans++;
         }
+        h--;
     }
 
     return ans;
 };
+
 // @lc code=end
 
